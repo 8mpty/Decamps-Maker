71b1efcf0cfc425629db8142eef16f0a4d46ac9d
Khai <contact@8mpty.com>
2026-03-06 15:09:51 +0800

    Added remaining incident types

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -9,5 +9,21 @@ class AppConstants {
     'HMV421',
   ];
 
-  static const List<String> caseOptions = ['Fire - DECAMS'];
+  static const List<String> caseOptions = ['Fire - DECAMS', 
+  'Fire - LARGE', 
+  'Fire - LARGE MRP', 
+  'Fire - MODERATE', 
+  'Fire - MINOR', 
+  'Fire - INVESTIGATION', 
+  'Fire - MOTOR VECHILES',
+  'Fire - TUNNEL',
+  'Rescue - RTA',
+  'Rescue - GAS LEAK', 
+  'Rescue -  INDUSTRIAL/DOMESTIC', 
+  'Rescue - TRAPPED DOOR', 
+  'Rescue - SUICIDE', 
+  'MOTOR VEHICLE ACCIDENT', 
+  'Medical - CARDIAC ARREST P1+'
+  'ENVIRONMENTAL OIL SPILLAGE',
+  'CBRE - CHEMICAL AGENT'];
 }
